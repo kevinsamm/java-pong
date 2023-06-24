2cb26425da3a885b835b3a0dfd112609bf390eb0
kevinsamm <kevinsammillan12@gmail.com>
2023-06-23 19:00:27 -0500

    First classes created (Main, Window and KeyListener).

---
+++ src/Window.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Window extends JFrame implements Runnable {
+    Graphics2D g2;
+    KL keyListener = new KL();
+    public Window() {
+        this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+        this.setTitle(Constants.SCREEN_TITLE);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(keyListener);
+        g2 = (Graphics2D) this.getGraphics();
+    }
+
+    public void update(double dt) {
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, 400);
+
+        if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
+            System.out.println("UP ARROW");
+        } else if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
+            System.out.println("DOWN ARROW");
+        }
+    }
+
+    @Override
+    public void run() {
+        double lastFrameTime = 0.0;
+        while (true) {
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
+
+            update(deltaTime);
+
+            try {
+                Thread.sleep(30);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+}
